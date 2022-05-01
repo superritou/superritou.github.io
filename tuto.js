86c310c8d63317009fbd1be5e82cb15816805194
superritou <102620856+superritou@users.noreply.github.com>
2022-05-01 12:40:49 +0200

    Update tuto.js

--- tuto.js
+++ tuto.js
@@ -1,5 +1,10 @@
 var pm = `	<ul id="context">
 		<li>
+			<a href="javascript:url(1336);">
+				Ajouter un bouton effacer les cookies &agrave; votre forum
+			</a>
+		</li>
+		<li>
 			<a href="javascript:url(1332);">
 				Afficher un message aux membres ayant moins de « X » messages
 			</a>
